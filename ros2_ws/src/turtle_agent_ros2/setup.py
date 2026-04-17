5f97cb287a8cc7f9246325de5151c03dfe7bb160
EvaWang007 <1297991500@qq.com>
2026-04-17 20:40:06 +0800

    Revert "chore: update turtle_agent_ros2 setup.py"
    This reverts commit c9bee240cb943668cd5046fab3f2b2add2d5f554.

--- ros2_ws/src/turtle_agent_ros2/setup.py
+++ ros2_ws/src/turtle_agent_ros2/setup.py
@@ -1,36 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-package_name = "turtle_agent_ros2"
+package_name = 'turtle_agent_ros2'
 
 setup(
     name=package_name,
-    version="0.1.0",
-    packages=find_packages(exclude=["test"]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
-        (f"share/{package_name}", ["package.xml"]),
-        (f"share/{package_name}/launch", ["launch/agent.launch.py"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=[
-        "setuptools",
-        "python-dotenv",
-        "langchain",
-        "langchain-core",
-        "langchain-community",
-        "langchain-openai",
-        "rich",
-        "pyinputplus",
-        "jpl-rosa",
-    ],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="you",
-    maintainer_email="you@example.com",
-    description="ROSA-based TurtleSim agent for ROS2",
-    license="Apache-2.0",
+    maintainer='evawang',
+    maintainer_email='1297991500@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
-        "console_scripts": [
-            "turtle_agent = turtle_agent_ros2.turtle_agent:main",
+        'console_scripts': [
         ],
     },
 )
-
